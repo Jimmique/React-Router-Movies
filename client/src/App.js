216c5e6eaad82802e375ff1cdc176c3e7b90dab9
Jimmique <jimmique88@gmail.com>
2019-08-09 20:11:28 -0700

    completed day 2 of the project

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
 import React, { useState } from 'react';
+import { Route } from 'react-router-dom';
 
 import SavedList from './Movies/SavedList';
-import { Route } from 'react-router-dom';
 import MovieList from './Movies/MovieList.js';
 import Movie from './Movies/Movie.js';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
 
+  const addToSavedList = movie => {
+    setSavedList( [...savedList, movie] );
+  };  
+
   return (
     <div>
       <SavedList list={savedList} />
-      <Route exact path="/" component={ MovieList } />
-      <Route path="/movies/2" component={ Movie } />
+      <Route exact path="/"render={ props => <MovieList {...props} /> } />
+      <Route path="/movies/:id" render={ props => <Movie {...props} addToSavedList={addToSavedList} /> } />
     </div>
   );
 };
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+import MovieCard from './MovieCard.js';
+
 const Movie = (props) => {
   const [movie, setMovie] = useState(null);
  
@@ -21,10 +23,10 @@ const Movie = (props) => {
   },[props.match.params.id]);
   
   // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+   const saveMovie = () => {
+     const addToSavedList = props.addToSavedList;
+     addToSavedList(movie)
+   }
 
   if (!movie) {
     return <div>Loading movie information...</div>;
@@ -33,23 +35,9 @@ const Movie = (props) => {
   const { title, director, metascore, stars } = movie;
   return (
     <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
+      <MovieCard {...props} movie={movie} />
 
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
-      <div className="save-button">Save</div>
+      <div className="save-button" onClick={saveMovie}>Save</div>
     </div>
   );
 }
